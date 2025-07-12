c571b52c1cd5d4aba6ec11c84a963c643ad21539
rex1008 <zhangrui.coder@gmail.com>
2025-07-12 23:37:50 +0800

    page query

--- src/app/api/articles/[id]/route.ts
+++ src/app/api/articles/[id]/route.ts
@@ -17,11 +17,26 @@ export async function DELETE(request: Request, { params }: IParams) {
   })
 }
 
-export async function PATCH(request: Request) {
-  
+export async function PATCH(request: Request, { params }: IParams) {
+  const data = await request.json()
+  let idx = -1
+  await db.update(({ posts }) => {
+    idx = posts.findIndex(item => item.id === params.id)
+    posts[idx] = { ...posts[idx], ...data }
+  })
+  return NextResponse.json({
+    code: 0,
+    message: "修改成功",
+    data: db.data.posts[idx]
+  })
 }
 
 // GET => /api/articles/:id
-export async function GET(request: Request) {
-  
+export async function GET(request: Request, { params }: IParams) {
+  const data = db.data.posts.find(item => item.id === params.id)
+  return NextResponse.json({
+    code: 0,
+    message: "查找成功",
+    data
+  })
 }
--- src/app/api/articles/route.ts
+++ src/app/api/articles/route.ts
@@ -1,9 +1,34 @@
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 import db from '@/db'
 
 
-export async function GET(request: Request) {
-  
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams
+  const pagenum =  Number(searchParams.get("pagenum")) || 1
+  const pagesize =  Number(searchParams.get("pagesize")) || 2
+  const query =  searchParams.get("query") || ""
+
+  const data = db.data.posts
+  let returnData = query ? data.filter(item => {
+    const { id, ...rest } = item
+    return Object.values(rest).some(value => String(value).toLowerCase().includes(query.toLowerCase()))
+  }) : data
+
+  const total = returnData.length
+
+  const startIndex = (pagenum - 1) * pagesize
+  const endIndex = Math.min(startIndex + pagesize, total)
+
+  returnData = startIndex >= total ? [] : returnData.slice(startIndex, endIndex)
+
+  return NextResponse.json({
+    code: 0,
+    message: "获取成功",
+    data: {
+      total,
+      list: returnData
+    }
+  })
 }
 
 export async function POST(request: Request) {
